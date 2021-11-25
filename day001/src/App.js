b49c803b1944edb61d5bf58a96d02b700bef250f
Yiming Pang <ympang@trip.com>
2021-11-25 10:23:23 +0800

    del unused var

--- day001/src/App.js
+++ day001/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState,useEffect} from 'react'
+import {useEffect} from 'react'
 import {getString, insertText} from './insertText'
 window.root =[{ type: 'p', children: [{ text: '' }] }]
 function App() {
